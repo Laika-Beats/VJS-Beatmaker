f5562b5052b3119264e6af9e6393c6b294537aa7
Laika-Beats <laikabeatsdenver@gmail.com>
2020-12-22 16:46:38 -0700

    play audio

--- assets/app.js
+++ assets/app.js
@@ -15,18 +15,34 @@ class DrumKit {
     // when the index gets to 8 then the next step will go to 0
     let step = this.index % 8;
     const activeBars = document.querySelectorAll(`.b${step}`);
-    // loops over the pads
+    // loops over the pads and applies an animation
     activeBars.forEach((bar) => {
-      bar.style.animation = `playTrack 0.3s alternate ease-in-out`;
+      bar.style.animation = `playTrack 0.3s alternate ease-in-out 2`;
+      if (bar.classList.contains("active")) {
+        //Check each sound
+        if (bar.classList.contains("kick-pad")) {
+          // resets the kick audio sample so it can be triggered again without the sample fully playing
+          this.kickAudio.currentTime = 0;
+          // plays the currently selected kick audio
+          this.kickAudio.play();
+        }
+        if (bar.classList.contains("snare-pad")) {
+          this.snareAudio.currentTime = 0;
+          this.snareAudio.play();
+        }
+        if (bar.classList.contains("hihat-pad")) {
+          this.hihatAudio.currentTime = 0;
+          this.hihatAudio.play();
+        }
+      }
     });
-
     this.index++;
   }
   start() {
-    // const interval = (60 / this.bpm) * 1000;
+    const interval = (30 / this.bpm) * 1000;
     setInterval(() => {
       this.repeat();
-    }, 1000);
+    }, interval);
   }
 }
 
@@ -34,6 +50,9 @@ const drumKit = new DrumKit();
 
 drumKit.pads.forEach((pad) => {
   pad.addEventListener("click", drumKit.activePad);
+  pad.addEventListener("animationend", function () {
+    this.style.animation = "";
+  });
 });
 
 drumKit.playBtn.addEventListener("click", function () {
